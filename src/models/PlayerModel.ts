304e00acb3dd90814a3efe29b3b57b3ef729ce59
6JoeB <55534489+6JoeB@users.noreply.github.com>
2020-02-16 16:47:31 +0000

    WAB-03 added player role to player model (#15)

--- src/models/PlayerModel.ts
+++ src/models/PlayerModel.ts
@@ -1,8 +1,12 @@
+import Enums from "../Enums";
+
 class Player {
     userName: string;
+    role: Enums.Role;
 
-    constructor(userName: string) {
+    constructor(userName: string, role: Enums.Role) {
         this.userName = userName;
+        this.role = role || Enums.Role.Standard;
     }
 }
 
