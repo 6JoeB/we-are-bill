c625aca2eeadc68a233254117f34e66fa4e08910
Tim Salva <tim@jtsalva.dev>
2020-02-16 21:13:16 +0000

    Make Player.role optional in constructor

--- src/models/PlayerModel.ts
+++ src/models/PlayerModel.ts
@@ -4,7 +4,7 @@ class Player {
     userName: string;
     role: Enums.Role;
 
-    constructor(userName: string, role: Enums.Role) {
+    constructor(userName: string, role?: Enums.Role) {
         this.userName = userName;
         this.role = role || Enums.Role.Standard;
     }
