ae30bc96b0cecbfd09300726f6eeacb77ae83288
6joeb <joe6ball@gmail.com>
2020-03-01 00:28:25 +0000

    testing tings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lobby from './Lobby';
 import Player from '../models/PlayerModel';
+import Colyseus from "colyseus.js";
 import './App.css';
+import State from "../../server/models/StateModel"
 
-const App = () => {
-    const players: Player[] = [new Player('Joe'), new Player('Joe'), new Player('Joe'), new Player('Joe')];
+const App = async () => {
+    const client = new Colyseus.Client("ws://localhost:2657");
+    const [players, setPlayers] = useState <Player[]>([]);
+
+    const room = await client.joinOrCreate<State>("game");
+    console.log("joined successfully", room.sessionId);
+
+    room.state.players.onAdd = function (player: Player, i) {
+        console.log("player joined!", player);
+    }
+
+    room.state.players.onRemove = function (player: Player, i) {
+        console.log("player left!", player);
+    }
+
+    room.onStateChange((state) => {
+        setPlayers(state.players as any as Player[])
+        console.log(`${room.sessionId} has a new state:`, state);
+    })
 
     return <div className="App">
         <Lobby players={players} />
